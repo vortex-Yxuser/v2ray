6642f8ba50153b08153917b2e2a5ad7068a2e5a3
vortex-Yxuser <vjfhfhggg@gmail.com>
2026-08-09 17:42:10 +0100

    Update HevTunnel.kt

--- app/src/main/java/com/simplesshvpn/app/HevTunnel.kt
+++ app/src/main/java/com/simplesshvpn/app/HevTunnel.kt
@@ -2,14 +2,24 @@ package com.simplesshvpn.app
 
 object HevTunnel {
 
-    init {
-        System.loadLibrary("hev-socks5-tunnel")
-    }
+init {
+    System.loadLibrary("hev-socks5-tunnel")
+}
 
-    external fun start(
-        config: String,
-        tunFd: Int
-    ): Int
+/**
+ * Starts TUN -> SOCKS5.
+ *
+ * This function blocks until the tunnel stops,
+ * so call it from a background thread.
+ *
+ * @param config Hev YAML configuration
+ * @param tunFd Android VpnService TUN file descriptor
+ */
+external fun start(
+    config: String,
+    tunFd: Int
+): Int
 
-    external fun stop()
+external fun stop()
+
 }
