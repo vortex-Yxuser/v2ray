a1b467375ac557e618a604f94e6339dc26f939ed
vortex-Yxuser <vjfhfhggg@gmail.com>
2026-08-09 17:30:17 +0100

    Update ConnectionConfig.kt

--- app/src/main/java/com/simplesshvpn/app/ConnectionConfig.kt
+++ app/src/main/java/com/simplesshvpn/app/ConnectionConfig.kt
@@ -3,23 +3,34 @@ package com.simplesshvpn.app
 import java.io.Serializable
 
 enum class ProxyType {
-    NONE, HTTP, HTTPS, SOCKS5
+    NONE,
+    HTTP,
+    HTTPS,
+    SOCKS5
 }
 
 data class ConnectionConfig(
     val sshHost: String,
     val sshPort: Int,
+
     val username: String,
+
     val password: String?,
     val privateKey: String?,
+
     val proxyType: ProxyType,
+
     val proxyHost: String?,
     val proxyPort: Int,
+
     val proxyUser: String?,
     val proxyPass: String?,
+
     val payload: String?
 ) : Serializable {
+
     companion object {
-        const val EXTRA_CONFIG = "extra_connection_config"
+        const val EXTRA_CONFIG =
+            "extra_connection_config"
     }
 }
